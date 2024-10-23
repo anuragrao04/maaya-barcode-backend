f3c8b910a14c40030fcbb179c8998069490e8cd2
anuragrao04 <raoanu2004@gmail.com>
2024-10-23 12:04:55 +0530

    added ||ism in sending requests

--- populateDB.py
+++ populateDB.py
@@ -1,33 +1,28 @@
 import csv
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 # URL for the POST request
-url = 'https://qr.ad-chaos.live/create'
+url = 'https://barcode.maaya-pes.co/create'
+
+
+def register_student(data):
+    response = requests.post(url, json=data)
+
+    if response.status_code != 200:
+        print(f"Registration failed for {data['name']}: {response.text}")
+
 
 with open('./student-deets.csv', 'r', encoding='utf-8-sig') as csvfile:
     reader = csv.DictReader(csvfile)
 
-    for row in reader:
-        srn = row['SRN/PRN']
-        prn = row['SRN/PRN']
+    student_data = [{
+        'srn': row['SRN'],
+        'prn': row['PRN'],
+        'name': row['Student Name'],
+        'semester': row['Sem'],
+        'branch': row['Branch']
+    } for row in reader]
 
-        srn_regex = r"^PES2UG\d{2}[A-Za-z]{2}\d{3}$"
-        prn_is_srn = re.match(regex, prn)
-
-        if (prn_is_srn):
-            prn = getPrnFromSrn(srn)
-
-        data = {
-            'srn': row['SRN'],
-            'prn': row['PRN'],
-            'name': row['Student Name'],
-            'semester': row['Sem'],
-            'branch': row['Branch']
-        }
-
-        response = requests.post(url, json=data)
-
-        if response.status_code == 200:
-            print(f"{data['name']} registered")
-        else:
-            print(f"fucked up for {data['name']}: {response.message}")
+with ThreadPoolExecutor(max_workers=8) as executor:
+    executor.map(register_student, student_data)
